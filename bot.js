21f51ea152f3f68fefc76dee3e0dce7e2f08b13d
Your Name <you@example.com>
2019-06-25 16:34:35 +0200

    nqme param

--- bot.js
+++ bot.js
@@ -19,24 +19,30 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-	axios.post('https://api.dialogflow.com/v1/query?v=20150910',
-	{  contexts: ["shop"],
-		 lang: "fr",
-		 query: message,
-		 sessionId: "f04e1bf8-18aa-4ab7-8832-58059591101e",
-		 timezone: "America/New_York"
-	},
-	{ headers: { 'Content-Type' : 'application/json', Authorization: "Bearer " + config.Dialogflow },
-	}).then(function(res){
-		try {
-			console.log(res.data.result.fulfillment.speech)
-			bot.sendMessage({
-					to: channelID,
-					message: res.data.result.fulfillment.speech + ' :kissing_smiling_eyes:'
-			});
-		}
-		catch(error){
-			console.log(error)
-		}
-	})
+	//console.log(bot)
+	if(user !== 'getPet'){
+		axios.post('https://api.dialogflow.com/v1/query?v=20150910',
+		{  contexts: ["shop"],
+			 lang: "fr",
+			 query: message + ' ' + user,
+			 sessionId: "f04e1bf8-18aa-4ab7-8832-58059591101e",
+			 timezone: "America/New_York"
+		},
+		{ headers: { 'Content-Type' : 'application/json', Authorization: "Bearer " + config.Dialogflow },
+		}).then(function(res){
+			try {
+				console.log(res.data.result.fulfillment.speech)
+				console.log(bot.channels[channelID])
+				//bot.channel.startTyping();
+				bot.sendMessage({
+						to: channelID,
+						message: res.data.result.fulfillment.speech
+				});
+				//bot.stopTyping();
+			}
+			catch(error){
+				console.log(error)
+			}
+		});
+	}
 });
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-	Dialogflow: '45daf4d9614c429382a2ecb8d632b7f4',
+	Dialogflow: '0100ccda86cc4b1681b2667f6cb74442',
 	Discord: 'ceYbVzFcoueI7Eey7oFR01TgLlYy7BpS'
 }
+// 45daf4d9614c429382a2ecb8d632b7f4
