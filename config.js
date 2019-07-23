a2e5b1d8ea559cf2c3b85955fdb94eea1b0fdc53
Your Name <you@example.com>
2019-07-23 17:28:57 +0200

    ok

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	Dialogflow: '0100ccda86cc4b1681b2667f6cb74442',
-	Discord: 'ceYbVzFcoueI7Eey7oFR01TgLlYy7BpS'
+	Dialogflow: '',
+	Discord: ''
 }
-// 45daf4d9614c429382a2ecb8d632b7f4
